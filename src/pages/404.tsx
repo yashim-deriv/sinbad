0a5d137a4f5e2a9205febe1c5293930599140af4
NikitK-deriv <nikita.kolesnik@deriv.com>
2022-06-20 09:05:35 +0300

    feat: start of 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
+import Layout from 'components/layout/layout'
+import { ContainerWrapper } from 'components/containers/common/style'
+
+const DescriptionContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    background-color: var(--color-sand-1);
+    min-height: 1000px;
+`
+
+const DescriptionContainerWrapper = styled(ContainerWrapper)`
+    padding: 50px 0;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+`
 
 const PageNotFound = () => {
-    return <h1>404</h1>
+    return (
+        <Layout>
+            <DescriptionContainer>
+                <DescriptionContainerWrapper>404</DescriptionContainerWrapper>
+            </DescriptionContainer>
+        </Layout>
+    )
 }
 
 export default PageNotFound
