cd6f9b6d390180d8a349ae70c33ac29bbd8c2ccd
NikitK-deriv <nikita.kolesnik@deriv.com>
2022-06-20 10:58:31 +0300

    feat: 404 page development

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,29 +1,91 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'gatsby'
 import Layout from 'components/layout/layout'
 import { ContainerWrapper } from 'components/containers/common/style'
+import ImageNotAvailable from 'images/common/sinbad-thinking.png'
 
 const DescriptionContainer = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: space-between;
-    background-color: var(--color-sand-1);
-    min-height: 1000px;
+    background-color: var(--color-white);
+    min-height: 700px;
 `
-
 const DescriptionContainerWrapper = styled(ContainerWrapper)`
     padding: 50px 0;
     display: flex;
     align-items: center;
     flex-direction: column;
 `
+const PageNotFoundContainerInfo = styled.div`
+    margin: 16px;
+
+    * {
+        margin: 1.6rem 0;
+    }
+`
+const PageNotFoundContainer = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    flex-direction: column;
+`
+const ButtonWrapper = styled.div`
+    margin-top: 3rem;
+    text-align: center;
+`
+const LinkButton = styled(Link)`
+    font-size: 2.4rem;
+    text-align: center;
+    text-decoration: none;
+    width: fit-content;
+    padding: 8px;
+    background-color: var(--color-sand-4);
+    border-radius: 8px;
+    color: var(--color-white);
+`
+
+const Header = styled.div`
+    font-weight: bold;
+    font-size: 3.2rem;
+    line-height: 4rem;
+    text-align: center;
+`
+
+const Text = styled.div`
+    font-size: 1.6rem;
+    line-height: 2.4rem;
+    text-align: center;
+`
 
 const PageNotFound = () => {
     return (
         <Layout>
             <DescriptionContainer>
-                <DescriptionContainerWrapper>404</DescriptionContainerWrapper>
+                <DescriptionContainerWrapper>
+                    <PageNotFoundContainer>
+                        <img
+                            src={ImageNotAvailable}
+                            alt="Page not found"
+                            width="600"
+                            height="600"
+                        />
+                        <PageNotFoundContainerInfo>
+                            <Header>We couldn’t find that page</Header>
+                            <Text>
+                                It may not be available in your country, or maybe a broken link has
+                                brought you here.
+                            </Text>
+                            <ButtonWrapper>
+                                <LinkButton to="/">Visit our homepage</LinkButton>
+                            </ButtonWrapper>
+                        </PageNotFoundContainerInfo>
+                    </PageNotFoundContainer>
+                </DescriptionContainerWrapper>
             </DescriptionContainer>
         </Layout>
     )
